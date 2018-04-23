d1dbf4b3b9651a50e8472d7b7b8a99a8870c117d
Alon Kodner <alon.kodner@bosun.co.nz>
2018-04-23 19:48:07 +1200

    First Commit

---
+++ src/main/java/co/nz/springboot/course/basics/springbootin10steps/BooksController.java
@@ -0,0 +1,15 @@
+package co.nz.springboot.course.basics.springbootin10steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BooksController {
+	@GetMapping("books")
+	public List<Book> getAllBooks() {
+		return Arrays.asList(new Book(1l,"test", "author test"));  
+	}
+}
